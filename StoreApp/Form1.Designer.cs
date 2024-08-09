2948e25ba25c35cab15224ea1b2041c1b2b3c6a5
Marcos Gualtero Lourenzo <marcosgualtero43253@gmail.com>
2024-08-09 17:32:37 -0300

    Update the grid sizes, the load image events and add a hiding columns method.

--- StoreApp/Form1.Designer.cs
+++ StoreApp/Form1.Designer.cs
@@ -29,29 +29,51 @@
         private void InitializeComponent()
         {
             this.dgvStore = new System.Windows.Forms.DataGridView();
+            this.pbxItem = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvStore)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxItem)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvStore
             // 
             this.dgvStore.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dgvStore.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvStore.Location = new System.Drawing.Point(24, 53);
+            this.dgvStore.ColumnHeadersHeight = 50;
+            this.dgvStore.Location = new System.Drawing.Point(12, 50);
+            this.dgvStore.MultiSelect = false;
             this.dgvStore.Name = "dgvStore";
-            this.dgvStore.Size = new System.Drawing.Size(585, 280);
+            this.dgvStore.ReadOnly = true;
+            this.dgvStore.RowTemplate.Height = 50;
+            this.dgvStore.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvStore.Size = new System.Drawing.Size(525, 300);
             this.dgvStore.TabIndex = 0;
+            this.dgvStore.SelectionChanged += new System.EventHandler(this.dgvStore_SelectionChanged);
+            // 
+            // pbxItem
+            // 
+            this.pbxItem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbxItem.Location = new System.Drawing.Point(559, 50);
+            this.pbxItem.MaximumSize = new System.Drawing.Size(229, 300);
+            this.pbxItem.MinimumSize = new System.Drawing.Size(229, 300);
+            this.pbxItem.Name = "pbxItem";
+            this.pbxItem.Size = new System.Drawing.Size(229, 300);
+            this.pbxItem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbxItem.TabIndex = 1;
+            this.pbxItem.TabStop = false;
             // 
             // frmStore
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbxItem);
             this.Controls.Add(this.dgvStore);
             this.Name = "frmStore";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Store App";
             this.Load += new System.EventHandler(this.frmStore_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvStore)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxItem)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -59,6 +81,7 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dgvStore;
+        private System.Windows.Forms.PictureBox pbxItem;
     }
 }
 
--- StoreApp/Form1.cs
+++ StoreApp/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using domain;
 using service;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace StoreApp
 {
@@ -26,10 +27,8 @@ namespace StoreApp
             {
                 itemList = service.list();
                 dgvStore.DataSource = itemList;
-                dgvStore.Columns["UrlImage"].Visible = false;
-                dgvStore.Columns["Description"].Visible = false;
-                dgvStore.Columns["Brand"].Visible = false;
-                dgvStore.Columns["Category"].Visible = false;
+                hidenColumns();
+                loadImage(itemList[0].UrlImage);
             }
             catch (Exception ex)
             {
@@ -40,6 +39,40 @@ namespace StoreApp
         private void frmStore_Load(object sender, EventArgs e)
         {
             load();
+        }
+        private void hidenColumns()
+        {
+            dgvStore.Columns["UrlImage"].Visible = false;
+            dgvStore.Columns["Description"].Visible = false;
+            dgvStore.Columns["Brand"].Visible = false;
+            dgvStore.Columns["Category"].Visible = false;
+        }
+        private void loadImage(string image)
+        {
+            try
+            {
+                pbxItem.Load(image);
+                if (pbxItem.Image.Width > 229 & pbxItem.Image.Height > 300)
+                    {
+                    pbxItem.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                else
+                {
+                    pbxItem.SizeMode = PictureBoxSizeMode.CenterImage;
+                }
+            }
+            catch (Exception ex)
+            {
+                pbxItem.SizeMode = PictureBoxSizeMode.StretchImage;
+                pbxItem.Load("https://media.istockphoto.com/vectors/no-image-available-icon-vector-id1216251206?k=20&m=1216251206&s=170667a&w=0&h=A72dFkHkDdSfmT6iWl6eMN9t_JZmqGeMoAycP-LMAw4=");
+            }
+        }
+
+        private void dgvStore_SelectionChanged(object sender, EventArgs e)
+        {
+            Item selected = (Item)dgvStore.CurrentRow.DataBoundItem;
+            loadImage(selected.UrlImage);
+
         }
     }
 }
